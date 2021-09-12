6cee6848c8c75bf4b13a8f768b5eeffb0cf88c29
sky-4934 <4934akash@gmail.com>
2021-09-12 17:06:58 +0530

    setup heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const weather = require('./utils/weather')
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 //Define paths for Express config
 const pub_dir = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -139,6 +141,6 @@ app.get('*',(req,res)=>{
 //app.com/help
 //app.com/about
 
-app.listen(3000, ()=>{
-    console.log('Server started');
+app.listen(port, ()=>{
+    console.log('Server started at .'+ port);
 })
